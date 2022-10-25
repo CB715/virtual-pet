c19323206a5e724ed1e97362fee3b828ebd1b20d
Carl Beckley <carlbeckley@gmail.com>
2022-10-25 16:06:09 +0100

    added tests and prototype to age the pet

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,7 +1,27 @@
 const Pet = require('../src/pet');
 
-describe('constructor', () => {
+describe('creating an object from a constructor', () => {
     it('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
   });
+
+describe('naming of pet', () => {
+    it('sets the name property', () => {
+      const pet = new Pet('Fido');
+      expect(pet.name).toEqual('Fido');
+    });
+  });
+
+  describe('age of pet', () => {
+    it('has an initial age of 0', () => {
+      const pet = new Pet('Fido');
+      expect(pet.age).toEqual(0);
+    });
+    it('increments the age of the pet by 1', () => {
+      const pet = new Pet('Fido');
+      pet.growUp();
+      expect(pet.age).toEqual(1);
+    });
+  });
+  
