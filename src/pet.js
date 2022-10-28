810670b5ed60e1701b235aa721a1f375851d82a4
Carl Beckley <carlbeckley@gmail.com>
2022-10-28 16:28:30 +0100

    added tests and function to check if pet is alive

--- src/pet.js
+++ src/pet.js
@@ -48,6 +48,21 @@ Pet.prototype.checkUp = function () {
         return ('I feel great!');
     };
 };
+Pet.prototype.isAlive = function () {
+    if(this.fitness <= 0) {
+        return false;
+    }
+    if(this.hunger >= 10) {
+        return false;
+    }
+    if(this.age >= 30) {
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+
 
 module.exports = Pet;
 
